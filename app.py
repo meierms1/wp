af99e63fbc56edf2282570d8511ba92c4dcec241
meierms1 <meierms@icloud.com>
2023-08-30 17:53:49 -0400

    testing speed performance

--- app.py
+++ app.py
@@ -144,4 +144,4 @@ def logout():
     return redirect("/about/")
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port = 5000)
+    app.run(debug=False, port:5000, host="0.0.0.0")
